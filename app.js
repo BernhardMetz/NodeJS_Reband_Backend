8280e546064ddb01a9bfa5f6caa3bfb063530920
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-03 21:03:55 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express=require('express')
-const app = express()
 const mongoose = require('mongoose')
-const mongo_uri = 'mongodb://localhost/rebrand';
+require('dotenv/config')
 
-mongoose.connect(mongo_uri, {useNewUrlParser: true}, (err) => {
+const loginRoute = require('./routes/login')
+
+const app = express()
+app.use('/api/login', loginRoute)
+app.use(express.json())
+
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, (err) => {
     if (err) throw err
     else console.log(`Successfully connected to ${mongo_uri}`)
 })
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
