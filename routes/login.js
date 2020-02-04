e9e0debf5802cdcf0727ccd9de3af0fcd9b3c0d6
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-04 09:27:23 +0100

    token was added

--- routes/login.js
+++ routes/login.js
@@ -7,14 +7,64 @@ router.get('/',  (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+    
+    const {email, password} = req.body
 
     try {
-        const savedUser = await user.save()
-        res.send(savedUser)
+        const searchedUser = await User.findOne({'email' : email}, (err, user) => {
+            if (err) {
+                console.error(err);
+                res.status(500)
+                  .json({
+                  error: 'Internal error please try again'
+                });
+            } else if (!user) {
+                res.status(401)
+                    .json({
+                        error: 'Incorrect email or password'
+                    });
+            } else {
+                user.isCorrectPassword(password, function(err, same) {
+                    if (err) {
+                        console.error(err);
+                        res.status(500)
+                            .json({
+                                error: 'Internal error please try again'
+                            });
+                    } else if (!same) {
+                        res.status(401)
+                            .json({
+                                error: 'Incorrect email or password'
+                            });
+                    } else {
+                        // Issue token
+          
+                        const cur_time=Date.now();
+                        const payload = {'email':email,'time':cur_time};
+                        console.log(email);
+                
+                
+                        const token = jwt.sign(payload, process.env.SECRET, {
+                            expiresIn: '1h'
+                        });
+                
+                        const token_item = new Token({ email,cur_time,token });
+                      
+                        token_item.save(function(err) {
+                            if (err) {
+                                console.log(err);
+                                res.status(500)
+                                    .send("Error logging in please try again.");
+                            } else {
+                                res.cookie('token', token, { httpOnly: true })
+                                    .status(200).send(token);
+                            }
+                        });
+                    }
+                });
+            }
+        })
+        res.send(searchedUser)
     } catch(err) {
         res.status(400).send({message: err})
     }
